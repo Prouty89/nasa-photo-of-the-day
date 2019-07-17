aa60a42fd417bbe6c1052fec195fd3d568cb9d9c
Blake <bpro0821@gmail.com>
2019-07-17 16:19:03 -0500

    Initial commit, install axios, create potential components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios'
 import "./App.css";
 
+// const [file, setFile] = useState();
+
+
+// console.log(file.date)
+
 function App() {
+  const [file, setFile] = useState();
+  useEffect(() => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=zZp02OALIOiVrMFtA2rwcnuYN4lnSFWMHQqX2BA6')
+    .then((info) => {
+      setFile(info.data)
+      console.log('API Data: ', info.data)
+    })
+    .catch(error => {
+      console.log('This API is not functional: ', error)
+    })
+  }, [])
+  console.log(file.date)
   return (
     <div className="App">
       <p>
